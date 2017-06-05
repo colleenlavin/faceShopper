f1db5bea1bf461567e0b0830af00b990246f4891
Fullstack friend <fullstackfriend@Mercedess-MacBook-Air.local>
2017-06-05 17:13:26 -0400

    Minor changes

--- db/models/thing.js
+++ db/models/thing.js
@@ -4,7 +4,7 @@ const {STRING} = require('sequelize')
 
 module.exports = db => db.define('things', {
   name: STRING,
-})     
+})
 
 module.exports.associations = (Thing, {User, Favorite}) => {
   Thing.belongsToMany(User, {as: 'lovers', through: Favorite})
