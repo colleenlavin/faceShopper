687e556581cbfdf7632f49e311726b0f719f76c9
colleenlavin <cml388@mun.ca>
2017-06-13 11:27:37 -0400

    styles

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -9,53 +9,19 @@ export default class Home extends Component {
 
     render() {
         return (
-
-
-            // <!-- Full Page Image Background Carousel Header -- >
-            <header id="myCarousel" className="carousel slide">
-                {/*<!-- Indicators -->*/}
-                <ol className="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol>
-
-                {/*<!-- Wrapper for Slides -->*/}
-                <div className="carousel-inner">
-                    <div className="item active">
-                        {/*<!-- Set the first background image using inline CSS below. -->*/}
-                        <div className="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
-                        <div className="carousel-caption">
-                            <h2>Caption 1</h2>
-                        </div>
-                    </div>
-                    <div className="item">
-                        {/*<!-- Set the second background image using inline CSS below. -->*/}
-                        <div className="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-                        <div className="carousel-caption">
-                            <h2>Caption 2</h2>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
-                        <div className="carousel-caption">
-                            <h2>Caption 3</h2>
-                        </div>
-                    </div>
+            <div>
+                <div className="col-md-6 " >
+                    <Link to={`/faces`}>
+                        <img className='img-item' src='./public/men' />
+                    </Link>
                 </div>
 
-                {/*<!-- Controls -->*/}
-                <a className="left carousel-control" href="#myCarousel" data-slide="prev">
-                    <span className="icon-prev"></span>
-                </a>
-                <a className="right carousel-control" href="#myCarousel" data-slide="next">
-                    <span className="icon-next"></span>
-                </a>
-
-            </header>
-
-
-
+                <div className="col-md-3 " >
+                    <Link to={`/faces`}>
+                        <img className='img-item' src='./public/woman' />
+                    </Link>
+                </div>
+            </div>
         )
     }
 
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from "react-router";
 import Login from './Login'
-// import WhoAmI from './WhoAmI'
 
 export default function Navbar() {
     return (
         <nav className="navbar navbar-inverse   navbar-fixed-top topnav " role="navigation">
             <div className="container topnav">
                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><Link id="home" to="/home">Home</Link></li>
+                        <li><Link id="faces" to="/faces">Faces</Link></li>
+                        <li><Link id="cart" to="/cart">Cart</Link></li>
+                    </ul>
                     <div className="nav navbar-nav navbar-left">
-                        <a><Link id="home" to="/home">Home</Link></a>
-                        <a><Link id="faces" to="/faces">Faces</Link></a>
-                    </div>
-                    <div className="nav navbar-nav navbar-right">
-                        <a><Link id="login" to="/login">Login</Link></a>
+                        <Link id="login" to="/login">Login</Link>
                     </div>
                 </div>
             </div>
@@ -21,4 +21,3 @@ export default function Navbar() {
        
     )
 }
-
