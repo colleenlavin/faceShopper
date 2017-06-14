4fbefce9d2f421018a8c0ba25843f2898d7aeff6
colleenlavin <cml388@mun.ca>
2017-06-14 09:07:43 -0400

    styles are added

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -9,14 +9,13 @@ export default function Navbar({handleClick}) {
         <nav className="navbar navbar-inverse   navbar-fixed-top topnav " role="navigation">
             <div className="container topnav">
                 <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul className="nav navbar-nav navbar-right">
+                    <ul className="nav navbar-nav navbar-left">
                         <li><Link id="home" to="/home">Home</Link></li>
                         <li><Link id="faces" to="/faces">Faces</Link></li>
                         <li><Link id="cart" to="/cart" onClick={handleClick}>Cart</Link></li>
+                        <li> <Link id="login" to="/login">Login</Link></li>
                     </ul>
-                    <div className="nav navbar-nav navbar-left">
-                        <Link id="login" to="/login">Login</Link>
-                    </div>
+                        
                 </div>
             </div>
         </nav>
